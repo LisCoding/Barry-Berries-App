a3a5485bde21ebd99079bbdd756b2b43b2fd0dfe
vinney <vinvivo@users.noreply.github.com>
2017-09-15 00:44:45 -0700

    added visual assets, refactored POI queries, input partial title/subtitle code

--- currentLocation/ViewController.swift
+++ currentLocation/ViewController.swift
@@ -10,12 +10,17 @@ import UIKit
 import CoreLocation
 import MapKit
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
     
-    var annotations: [MKAnnotation] = Array()
+    //    var annotations: [MKAnnotation] = Array()
+    
     
     @IBOutlet weak var mapView: MKMapView!
     let manager = CLLocationManager()
+    
+    // custom pin annotations
+    var pointAnnotation: CustomPointAnnotation!
+    var pinAnnotationView: MKPinAnnotationView!
     
     //add pointers
     
@@ -26,10 +31,15 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
         
+        // for custom pin annotations
+        mapView.delegate = self
+        mapView.mapType = MKMapType.standard
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let user_location = locations[0]
+        //added this code
+        manager.stopUpdatingLocation()
         //how much with want to zoom
         let span : MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
         
@@ -42,45 +52,65 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         
         
         
+        //making a request for parks  and groceries landmarks
         let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = "parks"
-        request.region = self.mapView.region
-        
-        
-        let search = MKLocalSearch(request: request)
-        search.start { (response, error) in
-            guard let response = response else {
-                print("Search error: \(String(describing: error))")
-                return
+        let locations = ["parks", "groceries"]
+        var count = 0
+        while count < 2 {
+            request.naturalLanguageQuery = locations[count]
+            //change
+            request.region = region
+            
+            
+            let search = MKLocalSearch(request: request)
+            search.start { (response, error) in
+                guard let response = response else {
+                    print("Search error: \(String(describing: error))")
+                    return
+                }
+                for item in response.mapItems {
+                    let annotation = MKPointAnnotation()
+                    annotation.title! = "You found a berry!"
+                    annotation.subtitle! = "Do you want to eat it? Tap to continue..."
+//                    print(annotation.title)
+//                    print(annotation.subtitle)
+                    annotation.coordinate.latitude = item.placemark.coordinate.latitude
+                    annotation.coordinate.longitude = item.placemark.coordinate.longitude
+                    //adding annotation to the map
+//                    self.mapView.addAnnotation(annotation)
+                    print("This  is my annotation", annotation)
+                    // custom pin annotations
+//                    self.pointAnnotation = CustomPointAnnotation()
+//                    self.pointAnnotation.pinCustomImageName = "berry"
+//                    self.pointAnnotation.coordinate = myLocation
+
+//                    self.pinAnnotationView = MKPinAnnotationView(annotation: self.pointAnnotation, reuseIdentifier: "pin")
+                    self.mapView.addAnnotation(annotation)
+                }
+                search.cancel()
             }
-            for item in response.mapItems {
-                let annotation = MKPointAnnotation()
-                annotation.coordinate.latitude = item.placemark.coordinate.latitude
-                annotation.coordinate.longitude = item.placemark.coordinate.longitude
-                self.mapView.addAnnotation(annotation)
-            }
-            search.cancel()
+            count += 1
+        }
+
+    }
+    
+    // Custom pin annotations
+    func mapAnnotationView(_ mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseIdentifier = "pin"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseIdentifier)
+            annotationView!.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            annotationView!.canShowCallout = true
+        } else {
+            annotationView!.annotation = annotation
         }
         
-        let request2 = MKLocalSearchRequest()
-        request2.naturalLanguageQuery = "grocery"
-        request2.region = self.mapView.region
+//        let customPointAnnotation = annotation as! CustomPointAnnotation
+        annotationView!.image = UIImage(named: "berry")
         
-        
-        let search2 = MKLocalSearch(request: request)
-        search2.start { (response, error) in
-            guard let response = response else {
-                print("Search error: \(String(describing: error))")
-                return
-            }
-            for item in response.mapItems {
-                let annotation = MKPointAnnotation()
-                annotation.coordinate.latitude = item.placemark.coordinate.latitude
-                annotation.coordinate.longitude = item.placemark.coordinate.longitude
-                self.mapView.addAnnotation(annotation)
-            }
-            search2.cancel()
-        }
+        return annotationView
     }
 }
 
