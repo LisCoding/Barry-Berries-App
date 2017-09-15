9adea5f61fb76b99f287fe934da725ec37ba1d03
pershanti <shantini29@gmail.com>
2017-09-14 21:24:27 -0700

    added markers from the POI search to the map

--- currentLocation/ViewController.swift
+++ currentLocation/ViewController.swift
@@ -11,59 +11,76 @@ import CoreLocation
 import MapKit
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-
+    
+    var annotations: [MKAnnotation] = Array()
+    
     @IBOutlet weak var mapView: MKMapView!
     let manager = CLLocationManager()
     
     //add pointers
-    let annotation = MKPointAnnotation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
         manager.requestWhenInUseAuthorization()
         manager.startUpdatingLocation()
-        let request = MKLocalSearchRequest()
-        request.naturalLanguageQuery = "grocery"
-        request.region = mapView.region
-        
-        let search = MKLocalSearch(request: request)
-        search.start { (response, error) in
-            guard let response = response else {
-                print("Search error: \(error)")
-                return
-            }
-            for item in response.mapItems {
-                print (item.placemark.coordinate.latitude) // add information to pins
-                print(item.name!)
-            }
-        }
-
-        mapView.addAnnotation(annotation)
-        
-        // Do any additional setup after loading the view, typically from a nib.
-        
         
     }
-
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+       func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let user_location = locations[0]
         //how much with want to zoom
         let span : MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
         
         //location of the user
         let myLocation:CLLocationCoordinate2D = CLLocationCoordinate2DMake(user_location.coordinate.latitude, user_location.coordinate.longitude)
-
-            
-            
-        annotation.coordinate = CLLocationCoordinate2D(latitude: (user_location.coordinate.latitude + 0.01), longitude: (user_location.coordinate.longitude + 0.01))
-//        annotation.coordinate = CLLocationCoordinate2D(latitude: (user_location.coordinate.latitude + 0.03), longitude: (user_location.coordinate.longitude + 0.03))
         
         let region : MKCoordinateRegion = MKCoordinateRegionMake(myLocation, span)
-        mapView.setRegion(region, animated: true)
-        mapView.showsUserLocation = true
+        self.mapView.setRegion(region, animated: true)
+        self.mapView.showsUserLocation = true
+        
+        
+        
+        let request = MKLocalSearchRequest()
+        request.naturalLanguageQuery = "grocery"
+        request.region = self.mapView.region
+        
+        
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response else {
+                print("Search error: \(String(describing: error))")
+                return
+            }
+            for item in response.mapItems {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate.latitude = item.placemark.coordinate.latitude
+                annotation.coordinate.longitude = item.placemark.coordinate.longitude
+                self.mapView.addAnnotation(annotation)
+            }
+            search.cancel()
+        }
+        
+        let request2 = MKLocalSearchRequest()
+        request2.naturalLanguageQuery = "grocery"
+        request2.region = self.mapView.region
+        
+        
+        let search2 = MKLocalSearch(request: request)
+        search2.start { (response, error) in
+            guard let response = response else {
+                print("Search error: \(String(describing: error))")
+                return
+            }
+            for item in response.mapItems {
+                let annotation = MKPointAnnotation()
+                annotation.coordinate.latitude = item.placemark.coordinate.latitude
+                annotation.coordinate.longitude = item.placemark.coordinate.longitude
+                self.mapView.addAnnotation(annotation)
+            }
+            search2.cancel()
+        }
     }
-    
-   
 }
 
